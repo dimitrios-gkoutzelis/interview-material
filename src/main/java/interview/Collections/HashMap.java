83402730f9447d08f63ef43304540f862883eeb8
raniants <ouranianoutsi@gmail.com>
2018-05-17 14:44:45 +0200

    hashMap implementation

---
+++ src/main/java/interview/Collections/HashMap.java
@@ -0,0 +1,35 @@
+package main.java.interview.basics.Collections;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Mary", "NewYork");
+        map.put("Tomas", "London");
+        map.put("Jan", "Rome");
+
+        System.out.println(map.get("Mary")); //get value for a specific key
+        System.out.println("keys:" + map.keySet()); //print all keys
+        System.out.println("values:" + map.values()); //print all values
+        System.out.println("entries:" + map.entrySet()); //print all entries
+        System.out.println("Our map contains an entry with key Mary: " + map.containsKey("Mary")); //true
+
+        Set setMap = map.entrySet();
+
+        //print all elements of Map using iterator
+        Iterator iterator = setMap.iterator();
+        while (iterator.hasNext()) {
+            Map.Entry mEntry = (Map.Entry) iterator.next();
+            System.out.println(mEntry.getKey() + "\n" + mEntry.getValue());
+        }
+
+        //print all elements of Map using for loop
+        for (Object aSetMap : setMap) {
+            Map.Entry mEntry = (Map.Entry) aSetMap;
+            System.out.println(mEntry.getKey() + "\n" + mEntry.getValue());
+        }
+    }
+}
